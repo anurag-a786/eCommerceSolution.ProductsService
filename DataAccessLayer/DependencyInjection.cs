474b8ffc3b02edf859a5ac06431e90f430db1eeb
wickerknox <wickerknox89@gmail.com>
2024-12-21 20:08:11 -0800

    Boilerplate Code

---
+++ DataAccessLayer/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.ProductsService.DataAccessLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services)
+        {
+            //TO DO: Add Business Logic Layer services into the IoC container
+
+
+            return services;
+        }
+    }
+}
+
