496f44d7efad0cd600d681e8e987f01def32cdae
wickerknox <wickerknox89@gmail.com>
2025-03-10 21:18:27 -0700

    Async Microservice Communication using RabbitMQ : RabbitMQ NuGET Package

---
+++ BusinessLogicLayer/RabbitMQ/IRabbitMQPublisher.cs
@@ -0,0 +1,9 @@
+﻿namespace eCommerce.ProductsService.BusinessLogicLayer.RabbitMQ
+{
+    public interface IRabbitMQPublisher
+    {
+        void Publish<T>(string routingKey, T message);
+    }
+}
+
+
