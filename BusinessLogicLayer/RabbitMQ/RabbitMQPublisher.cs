26ae4950a44672ecfa84125646b69368e64e3f30
wickerknox <wickerknox89@gmail.com>
2025-03-11 17:33:14 -0700

    Async Microservice Communication using RabbitMQ : RabbitMQ Connection

--- BusinessLogicLayer/RabbitMQ/RabbitMQPublisher.cs
+++ BusinessLogicLayer/RabbitMQ/RabbitMQPublisher.cs
@@ -1,20 +1,35 @@
 ﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
 
 namespace eCommerce.ProductsService.BusinessLogicLayer.RabbitMQ
 {
-    public class RabbitMQPublisher : IRabbitMQPublisher
+    public class RabbitMQPublisher : IRabbitMQPublisher, IDisposable
     {
         private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
 
         public RabbitMQPublisher(IConfiguration configuration)
         {
             _configuration = configuration;
-
             string hostName = _configuration["RabbitMQ_HostName"]!;
             string userName = _configuration["RabbitMQ_UserName"]!;
             string password = _configuration["RabbitMQ_Password"]!;
             string port = _configuration["RabbitMQ_Port"]!;
 
+            ConnectionFactory connectionFactory = 
+                new ConnectionFactory() { HostName = hostName, UserName = userName, 
+                                          Password = password, Port = Convert.ToInt32(port)
+            };
+
+            _connection = connectionFactory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            _channel.Dispose();
+            _connection.Dispose();
         }
 
         public void Publish<T>(string routingKey, T message)
